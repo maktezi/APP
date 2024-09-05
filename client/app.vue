3325eccab7f00e5dc9789bcd281178b0014da877
maktezi <mlcnierras@gmail.com>
2024-09-05 13:26:13 +0800

    fix: page animation

--- client/app.vue
+++ client/app.vue
@@ -23,13 +23,11 @@ const height = 5;
 </script>
 
 <style>
-.page-enter-active,
 .page-leave-active {
     transition: all 0.3s;
 }
-.page-enter-from,
 .page-leave-to {
     opacity: 0;
-    filter: blur(0.3rem);
+    filter: blur(0.2rem);
 }
 </style>
