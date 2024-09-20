7e567a8cf001134b5c77ad2bc3ca25d997ea51a4
marknierras <mlcnierras@gmail.com>
2024-09-20 21:31:24 +0800

    fix: update user table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('first_name')->nullable();
-            $table->string('_middle_name')->nullable();
+            $table->string('middle_name')->nullable();
             $table->string('last_name')->nullable();
             $table->string('complete_name')->nullable();
             $table->string('email')->unique();
