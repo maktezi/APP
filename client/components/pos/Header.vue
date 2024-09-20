bc3083444adf90fb44496593cb12c4f5bc5c4e8e
marknierras <mlcnierras@gmail.com>
2024-09-21 00:39:04 +0800

    feat: update header component

--- client/components/pos/Header.vue
+++ client/components/pos/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="flex m-auto items-center justify-between bg-red-950 p-2 rounded mb-1.5"
+        class="flex m-auto items-center justify-between bg-red-900 dark:bg-red-950 p-2 rounded mb-1.5"
     >
         <div class="flex items-center gap-2 justify-center">
             <TooltipProvider>
