62e7ce92bdeda1a311aaee5f8005d2e3ec232670
marknierras <mlcnierras@gmail.com>
2024-09-21 14:53:45 +0800

    fix: product pos header

--- client/components/pos/product/Header.vue
+++ client/components/pos/product/Header.vue
@@ -7,7 +7,13 @@
             <p class="text-2xl font-bold py-1">Products</p>
         </div>
         <div class="flex items-center gap-1">
-            <div class="w-[215px] md:w-[320px] flex items-center relative">
+            <span
+                class="flex items-center bg-gray-300 dark:bg-gray-800 p-1 hover:bg-gray-500 justify-center rounded-full"
+                @click="toggleView()"
+            >
+                <Icon :name="name" class="toggle-icon" size="30" />
+            </span>
+            <div class="flex items-center relative">
                 <span
                     ><Icon
                         name="mdi-search"
@@ -16,16 +22,9 @@
                 /></span>
                 <input
                     placeholder="Search"
-                    class="pl-9 py-2 xs:w-1/3 md:w-full dark:bg-gray-700 text-gray-900 rounded-xl outline-none"
+                    class="pl-9 py-2 dark:bg-gray-700 text-gray-900 rounded-xl outline-none"
                 />
             </div>
-            <Button
-                variant="outline"
-                class="flex items-center hover:bg-gray-500 justify-center rounded-full"
-                @click="toggleView()"
-            >
-                <Icon :name="name" class="toggle-icon" size="20" />
-            </Button>
         </div>
     </div>
 </template>
