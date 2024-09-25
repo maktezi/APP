cf51b41b60c4a0b0522d68a1e3c8f61040b0f7ba
MkTz <94935785+maktezi@users.noreply.github.com>
2024-09-25 22:38:25 +0800

    fix: sidebar (#18)

--- client/components/AppSidebar.vue
+++ client/components/AppSidebar.vue
@@ -18,26 +18,26 @@
                         <p class="font-bold text-xl">APP</p>
                     </div>
                 </div>
-            </SheetHeader>
-            <SheetClose class="w-full mt-4">
-                <NuxtLink
-                    v-for="(link, index) in Object.values(links)"
-                    :key="index"
-                    :to="link.path"
-                    class="flex w-full items-center gap-4 px-4 py-3 transition rounded-md cursor-pointer hover:bg-red-400/80 dark:hover:bg-red-900/80"
-                >
-                    <Icon
-                        size="20"
-                        :name="link.icon"
-                        :class="link.iconColor"
-                    />
-                    <span
-                        class="text-sm font-semibold"
-                        :class="link.textColor"
-                        >{{ link.title }}</span
+                <SheetClose class="w-full mt-4">
+                    <NuxtLink
+                        v-for="(link, index) in Object.values(links)"
+                        :key="index"
+                        :to="link.path"
+                        class="flex w-full items-center gap-4 px-4 py-3 transition rounded-md cursor-pointer hover:bg-red-400/80 dark:hover:bg-red-900/80"
                     >
-                </NuxtLink>
-            </SheetClose>
+                        <Icon
+                            size="20"
+                            :name="link.icon"
+                            :class="link.iconColor"
+                        />
+                        <span
+                            class="text-sm font-semibold"
+                            :class="link.textColor"
+                            >{{ link.title }}</span
+                        >
+                    </NuxtLink>
+                </SheetClose>
+            </SheetHeader>
         </SheetContent>
     </Sheet>
 </template>
