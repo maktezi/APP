99c974892b2cf494f4047856ef11c0835d83a8fc
marknierras <mlcnierras@gmail.com>
2024-09-28 13:15:13 +0800

    refactor: components

--- client/components/AppSidebar.vue
+++ client/components/AppSidebar.vue
@@ -1,45 +1,47 @@
 <template>
-    <Sheet>
-        <SheetTrigger class="flex items-center justify-center">
-            <!--            <Icon name="mdi:menu" size="30" class="text-gray-100" /> -->
-            <Icon name="mdi:menu" size="30" class="text-gray-100" />
-        </SheetTrigger>
+    <div>
+        <Sheet>
+            <SheetTrigger class="flex items-center justify-center">
+                <!--            <Icon name="mdi:menu" size="30" class="text-gray-100" /> -->
+                <Icon name="mdi:menu" size="30" class="text-gray-100" />
+            </SheetTrigger>
 
-        <SheetContent
-            side="left"
-            class="sm:w-80 w-full border-transparent sm:border-0 rounded-none md:rounded-r-lg"
-        >
-            <SheetHeader>
-                <SheetTitle class="hidden" />
-                <SheetDescription class="hidden" />
-                <div class="flex items-center justify-between">
-                    <div class="flex items-end gap-2">
-                        <Logo />
-                        <p class="font-bold text-xl">APP</p>
+            <SheetContent
+                side="left"
+                class="sm:w-80 w-full border-transparent sm:border-0 rounded-none md:rounded-r-lg"
+            >
+                <SheetHeader>
+                    <SheetTitle class="hidden" />
+                    <SheetDescription class="hidden" />
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-end gap-2">
+                            <Logo />
+                            <p class="font-bold text-xl">APP</p>
+                        </div>
                     </div>
-                </div>
-                <SheetClose class="w-full mt-4">
-                    <NuxtLink
-                        v-for="(link, index) in Object.values(links)"
-                        :key="index"
-                        :to="link.path"
-                        class="flex w-full items-center gap-4 px-4 py-3 transition rounded-md cursor-pointer hover:bg-red-400/80 dark:hover:bg-red-900/80"
-                    >
-                        <Icon
-                            size="20"
-                            :name="link.icon"
-                            :class="link.iconColor"
-                        />
-                        <span
-                            class="text-sm font-semibold"
-                            :class="link.textColor"
-                            >{{ link.title }}</span
+                    <SheetClose class="w-full mt-4">
+                        <NuxtLink
+                            v-for="(link, index) in Object.values(links)"
+                            :key="index"
+                            :to="link.path"
+                            class="flex w-full items-center gap-4 px-4 py-3 transition rounded-md cursor-pointer hover:bg-red-400/80 dark:hover:bg-red-900/80"
                         >
-                    </NuxtLink>
-                </SheetClose>
-            </SheetHeader>
-        </SheetContent>
-    </Sheet>
+                            <Icon
+                                size="20"
+                                :name="link.icon"
+                                :class="link.iconColor"
+                            />
+                            <span
+                                class="text-sm font-semibold"
+                                :class="link.textColor"
+                                >{{ link.title }}</span
+                            >
+                        </NuxtLink>
+                    </SheetClose>
+                </SheetHeader>
+            </SheetContent>
+        </Sheet>
+    </div>
 </template>
 
 <script setup lang="ts">
--- client/components/DisplayMode.vue
+++ client/components/DisplayMode.vue
@@ -1,30 +1,32 @@
 <template>
-    <DropdownMenu>
-        <DropdownMenuTrigger as-child>
-            <Button variant="secondary" class="rounded-full p-3">
-                <Icon
-                    icon="tdesign:mode-light"
-                    class="h-[1rem] w-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-                />
-                <Icon
-                    icon="tdesign:mode-dark"
-                    class="absolute h-[1rem] w-[1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-                />
-                <span class="sr-only">Toggle theme</span>
-            </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-            <DropdownMenuItem @click="colorMode.preference = 'light'">
-                Light
-            </DropdownMenuItem>
-            <DropdownMenuItem @click="colorMode.preference = 'dark'">
-                Dark
-            </DropdownMenuItem>
-            <DropdownMenuItem @click="colorMode.preference = 'system'">
-                System
-            </DropdownMenuItem>
-        </DropdownMenuContent>
-    </DropdownMenu>
+    <div>
+        <DropdownMenu>
+            <DropdownMenuTrigger as-child>
+                <Button variant="secondary" class="rounded-full p-3">
+                    <Icon
+                        icon="tdesign:mode-light"
+                        class="h-[1rem] w-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+                    />
+                    <Icon
+                        icon="tdesign:mode-dark"
+                        class="absolute h-[1rem] w-[1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+                    />
+                    <span class="sr-only">Toggle theme</span>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuItem @click="colorMode.preference = 'light'">
+                    Light
+                </DropdownMenuItem>
+                <DropdownMenuItem @click="colorMode.preference = 'dark'">
+                    Dark
+                </DropdownMenuItem>
+                <DropdownMenuItem @click="colorMode.preference = 'system'">
+                    System
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    </div>
 </template>
 
 <script setup lang="ts">
