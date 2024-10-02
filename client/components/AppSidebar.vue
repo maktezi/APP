f38fef3c2d51dbf57dbb741a49241cbac4d4bc07
MkTz <94935785+maktezi@users.noreply.github.com>
2024-10-02 21:47:07 +0800

    fix: sidebar (#34)

--- client/components/AppSidebar.vue
+++ client/components/AppSidebar.vue
@@ -10,34 +10,35 @@
                 class="sm:w-80 w-full border-transparent sm:border-0 rounded-none md:rounded-r-lg"
             >
                 <SheetHeader>
-                    <SheetTitle class="hidden" />
-                    <SheetDescription class="hidden" />
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-end gap-2">
-                            <Logo />
-                            <p class="font-bold text-xl">APP</p>
+                    <SheetTitle>
+                        <div class="flex items-center justify-between">
+                            <div class="flex items-end gap-2">
+                                <Logo />
+                                <p class="font-bold text-xl">APP</p>
+                            </div>
                         </div>
-                    </div>
-                    <SheetClose class="w-full mt-4">
-                        <NuxtLink
-                            v-for="(link, index) in links"
-                            :key="index"
-                            :to="link.path"
-                            class="flex w-full items-center gap-4 px-4 py-3 transition rounded-md cursor-pointer hover:bg-red-400/80 dark:hover:bg-red-900/80"
-                        >
-                            <Icon
-                                size="20"
-                                :name="link.icon"
-                                :class="link.iconColor"
-                            />
-                            <span
-                                class="text-sm font-semibold"
-                                :class="link.textColor"
-                                >{{ link.title }}</span
-                            >
-                        </NuxtLink>
-                    </SheetClose>
+                    </SheetTitle>
+                    <SheetDescription class="hidden" />
                 </SheetHeader>
+                <SheetClose class="w-full mt-4">
+                    <NuxtLink
+                        v-for="(link, index) in links"
+                        :key="index"
+                        :to="link.path"
+                        class="flex w-full items-center gap-4 px-4 py-3 transition rounded-md cursor-pointer hover:bg-red-400/80 dark:hover:bg-red-900/80"
+                    >
+                        <Icon
+                            size="20"
+                            :name="link.icon"
+                            :class="link.iconColor"
+                        />
+                        <span
+                            class="text-sm font-semibold"
+                            :class="link.textColor"
+                            >{{ link.title }}</span
+                        >
+                    </NuxtLink>
+                </SheetClose>
             </SheetContent>
         </Sheet>
     </div>
