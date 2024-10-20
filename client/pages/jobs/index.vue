29966cb87e1e63b44e25c6150fedeef71a225fb6
marknierras <mlcnierras@gmail.com>
2024-10-20 16:07:55 +0800

    refactor: table width

--- client/pages/jobs/index.vue
+++ client/pages/jobs/index.vue
@@ -3,7 +3,7 @@
         <Head>
             <Title>Jobs</Title>
         </Head>
-        <main v-auto-animate class="max-w-screen-xl mx-auto">
+        <main v-auto-animate class="max-w-screen-2xl mx-auto">
             <TableHeader title="Jobs">
                 <template #actions>
                     <TableCRUD />
--- client/pages/settings/promotions.vue
+++ client/pages/settings/promotions.vue
@@ -4,7 +4,7 @@
             <Title>{{ pageTitle }}</Title>
         </Head>
 
-        <main v-auto-animate class="max-w-screen-xl mx-auto h-[780px]">
+        <main v-auto-animate class="max-w-screen-2xl mx-auto h-[780px]">
             <TableHeader :title="pageTitle" :icon="icon" :search-bar="false" />
             <div
                 class="flex w-full justify-start flex-col items-center gap-1 p-4 border-2 border-secondary dark:border-primary min-h-[750px]"
