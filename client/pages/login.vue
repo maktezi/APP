c39bb21d83ec974d39f5fb31942030c04b31a595
marknierras <mlcnierras@gmail.com>
2024-10-21 22:00:07 +0800

    refactor: login page

--- client/pages/login.vue
+++ client/pages/login.vue
@@ -1,86 +1,96 @@
 <template>
-  <div class="min-h-screen flex flex-col justify-center bg-gray-900">
-    <div class="container mx-auto flex flex-col md:flex-row items-center justify-center p-6 md:p-10 space-y-10 md:space-y-0">
-      <!-- Logo and Welcome Text Section -->
-      <div class="md:flex-1 flex flex-col items-center text-center md:text-left px-4">
-        <img src="../assets/no-image.jpg" alt="Logo" class="mb-8 w-48 md:w-64" />
-        <h1 class="text-3xl md:text-4xl font-bold text-white mb-4">POS SYSTEM</h1>
-        <p class="text-gray-400 max-w-md">
-          Welcome to POS System! Please log in to streamline your
-          <span class="text-yellow-500">sales, manage inventory,</span>
-          and enhance your <span class="text-yellow-500">customer experience.</span> Let's get started!
-        </p>
-      </div>
+    <div class="min-h-screen flex flex-col justify-center bg-gray-900/20">
+        <div
+            class="container bg-transparent md:bg-gray-800/70 rounded-xl mx-auto flex flex-col md:flex-row items-center justify-center p-6 md:p-36 md:space-y-0"
+        >
+            <div
+                class="md:flex-1 flex flex-col items-center text-center md:text-left p-2 mb-4"
+            >
+                <img
+                    src="../assets/application_logo.svg"
+                    alt="Logo"
+                    class="mb-2 w-24 md:w-48"
+                />
+                <h1 class="text-3xl md:text-4xl font-bold text-white mb-2">
+                    POS SYSTEM
+                </h1>
+                <p class="text-gray-200 max-w-md">
+                    Welcome to POS System! Please log in to streamline your
+                    <span class="text-emerald-400"
+                        >sales, manage inventory,</span
+                    >
+                    and enhance your
+                    <span class="text-emerald-400">customer experience.</span>
+                    Let's get started!
+                </p>
+            </div>
 
-      <!-- Login Card Section -->
-      <div class="md:flex-1 max-w-full md:max-w-sm bg-gray-800 p-6 md:p-8 rounded-lg shadow-lg mx-4 md:mx-0">
-        <h2 class="text-2xl font-semibold text-center text-white mb-6">Sign In</h2>
-        <form @submit.prevent="login">
-          <!-- Username Field -->
-          <div class="mb-4">
-            <label for="email" class="text-gray-300">Username</label>
-            <input
-              id="email"
-              v-model="credentials.email"
-              type="email"
-              class="block w-full mt-1 p-3 md:p-4 bg-gray-700 text-white border-none rounded-md focus:ring-yellow-500 focus:ring-2 text-base md:text-lg placeholder-gray-400"
-              placeholder="Enter your username"
-              required
-              autofocus
-            />
-          </div>
+            <div
+                class="md:flex-1 max-w-full w-[400px] md:max-w-lg bg-gray-900/50 p-12 rounded-lg shadow-lg mx-4 md:mx-0"
+            >
+                <h2 class="text-2xl font-semibold text-center text-white mb-6">
+                    Sign In
+                </h2>
+                <div class="mb-4">
+                    <label for="email" class="text-gray-300">Username</label>
+                    <input
+                        id="email"
+                        v-model="credentials.email"
+                        type="email"
+                        class="block w-full mt-1 p-2 px-3 bg-gray-700 text-white border-none rounded-md focus:ring-yellow-500 focus:ring-2 text-base placeholder-gray-400"
+                        placeholder="Enter your username"
+                        required
+                        autofocus
+                    />
+                </div>
 
-          <!-- Password Field -->
-          <div class="mb-6">
-            <label for="password" class="text-gray-300">Password</label>
-            <input
-              id="password"
-              v-model="credentials.password"
-              type="password"
-              class="block w-full mt-1 p-3 md:p-4 bg-gray-700 text-white border-none rounded-md focus:ring-yellow-500 focus:ring-2 text-base md:text-lg placeholder-gray-400"
-              placeholder="Enter your password"
-              required
-            />
-          </div>
+                <div class="mb-6">
+                    <label for="password" class="text-gray-300">Password</label>
+                    <input
+                        id="password"
+                        v-model="credentials.password"
+                        type="password"
+                        class="block w-full mt-1 p-2 bg-gray-700 px-3 text-white border-none rounded-md focus:ring-yellow-500 focus:ring-2 text-base placeholder-gray-400"
+                        placeholder="Enter your password"
+                        required
+                    />
+                </div>
 
-          <!-- Remember Me Checkbox -->
-          <div class="flex items-center justify-between mb-6">
-            <label for="remember" class="inline-flex items-center">
-              <input
-                id="remember"
-                v-model="credentials.remember"
-                type="checkbox"
-                class="text-yellow-500 border-gray-300 rounded focus:ring-yellow-500"
-              />
-              <span class="ml-2 text-sm text-gray-400">Remember me</span>
-            </label>
-          </div>
+                <div class="flex items-center justify-between mb-6">
+                    <label for="remember" class="inline-flex items-center">
+                        <input
+                            id="remember"
+                            v-model="credentials.remember"
+                            type="checkbox"
+                            class="text-yellow-500 border-gray-300 rounded focus:ring-yellow-500"
+                        />
+                        <span class="ml-2 text-sm text-gray-400"
+                            >Remember me</span
+                        >
+                    </label>
+                </div>
 
-          <!-- Submit Button -->
-          <Button
-            :disabled="loading"
-            class="w-full bg-primary mt-6"
-            @click.prevent="login"
-          >
-            <SpinnerTadpole
-              :class="{ hidden: !loading }"
-              class="size-7 text-card dark:text-card-foreground mx-1"
-            />
-            <span
-              class="font-bold"
-              :class="{ 'animate-pulse ml-2': loading }"
-            >{{ loading ? 'Logging in' : 'Login' }}</span>
-          </Button>
-        </form>
-      </div>
+                <Button
+                    :disabled="loading"
+                    class="w-full bg-emerald-700"
+                    @click.prevent="login"
+                >
+                    <SpinnerTadpole
+                        :class="{ hidden: !loading }"
+                        class="size-7 text-card dark:text-card-foreground mx-1"
+                    />
+                    <span
+                        class="font-bold"
+                        :class="{ 'animate-pulse ml-2': loading }"
+                        >{{ loading ? 'Logging in' : 'Login' }}</span
+                    >
+                </Button>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup lang="ts">
-import { ref, reactive } from 'vue';
-import { useRouter } from 'vue-router';
-
 const auth = useAuth();
 const loading = ref(false);
 const errors = ref(null);
@@ -102,8 +112,10 @@ const login = async () => {
         await auth.getUser();
         router.push('/dashboard');
     } catch (error: any) {
-        console.error(error); // Log the full error for debugging
-        const message = error.response?.data?.message || 'An error occurred. Please try again.';
+        console.error(error);
+        const message =
+            error.response?.data?.message ||
+            'An error occurred. Please try again.';
         toasts(message, {
             type: 'error',
             position: 'top-center',
@@ -112,7 +124,7 @@ const login = async () => {
             hideProgressBar: true,
         });
     } finally {
-        loading.value = false; // Always stop loading
+        loading.value = true;
     }
 };
 
